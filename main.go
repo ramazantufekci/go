54cd39a79bb37f0f3c7b59b8d8ed8db5a654a9bf
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2024-10-22 15:02:07 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
   fmt.Println("##### İlk uygulama ####")
   fmt.Println("Todolist App")
+
+  var taskItems = []string{"Görev 1", "Görev 2", "Görev 3"}
+
+  for index, task := range taskItems{
+    fmt.Println(task)
+  }
 }
