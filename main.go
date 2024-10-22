4315253665be5ba8c3b9fbfb6eb2130828995952
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2024-10-22 16:25:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import "fmt"
-var taskItems = []string{"Görev 1", "Görev 2", "Görev 3"}
+
 func main() {
   fmt.Println("##### İlk uygulama ####")
   fmt.Println("Todolist App")
-
+  var taskItems = []string{"Görev 1", "Görev 2", "Görev 3"}
   
-  printTasks()
+  printTasks(taskItems)
   
 }
 
-func printTasks(){
+func printTasks(taskItems []string){
   for index, task := range taskItems{
     //fmt.Println(task)
     fmt.Printf("%d. %s\n",index+1,task)
