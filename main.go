270732e6fc1a43669428000fe5a3985872a3ad91
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2024-10-22 17:20:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var taskItems = []string{"Görev 1", "Görev 2", "Görev 3"}
 
 func main() {
-  fmt.Println("##### İlk uygulama ####")
-  fmt.Println("Todolist App")
-  var taskItems = []string{"Görev 1", "Görev 2", "Görev 3"}
-  
+	fmt.Println("##### İlk uygulama ####")
+	http.HandleFunc("/",helloUser)
+	http.HandleFunc("/show-tasks",showTasks)
+	http.ListenAndServe(":8082",nil)
+   
   printTasks(taskItems)
   fmt.Println()
-  addTask(taskItems, "Go for a run")
+  taskItems = addTask(taskItems, "Go for a run")
+  taskItems = addTask(taskItems, "Pratik yapıyoruz Go")
+  printTasks(taskItems)
   
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request){
+	fmt.Fprintln(writer,taskItems)
+}
+
+func helloUser(writer http.ResponseWriter, request *http.Request){
+	var greeting = "Hello user. Welcome to our Todolist App!"
+	fmt.Fprintln(writer,greeting)
 }
 
 func printTasks(taskItems []string){
@@ -20,8 +37,8 @@ func printTasks(taskItems []string){
   }
 }
 
-func addTask(taskItems []string, newTask string){
+func addTask(taskItems []string, newTask string) []string {
 
   var updatedTaskItems = append(taskItems,newTask)
-  printTasks(updatedTaskItems)
+  return updatedTaskItems
 }
